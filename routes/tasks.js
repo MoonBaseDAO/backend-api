0d694e98cd95214da00683d7f35fccd710bd7558
DaneBennettWalter <69996337+DaneBennettWalter@users.noreply.github.com>
2023-03-14 14:46:54 -0500

    Create tasks.js

---
+++ routes/tasks.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { getAllTasks, getTaskById, createTask, updateTask, deleteTask } = require('../controllers/tasks');
+
+// define routes
+router.get('/', getAllTasks);
+router.get('/:id', getTaskById);
+router.post('/', createTask);
+router.put('/:id', updateTask);
+router.delete('/:id', deleteTask);
+
+module.exports = router;
